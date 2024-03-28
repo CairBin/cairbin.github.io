a1638e601b71399bbbaaa1bd283f1b42bf5f804c
CairBin <cairbin@CairBindeMacBook-Pro.local>
2024-03-29 01:14:59 +0800

    commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# TileIndex
+
+## 介绍
+
+这是我使用Vue写的仿Windows10样式的磁贴个人Web索引页，用于部署到Github Page上
+
+展示网站https://cairbin.github.io
+
+## 实现功能
+
+* 磁贴样式
+* 响应式布局
+* 调用一言接口
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,10 +1,19 @@
 <script setup>
 import { ref,reactive, onMounted } from "vue";
+import axios from 'axios'
+
+const api = axios.create({
+    headers:{
+        'Content-Type':'application/json',
+    },
+    timeout:10000
+})
 
 const typeWriterRef = ref()
 
 const data = reactive({
-    bannerContent:''
+    bannerContent:'',
+    oneSentence:''
 })
 
 let index = 0;
@@ -16,8 +25,36 @@ const typeWriterFunc = ()=>{
     }
 }
 
+/*
+{
+  "code": 200,
+  "data": {
+    "id": 76,
+    "tag": "生活",
+    "name": "佚名",
+    "origin": "木芽",
+    "content": "有些东西可以留下，例如服务器操作日志，数据库操作日志。有些东西转身就删了，例如聊天记录，每天清除一切软件的聊天记录，是我每天乐于干的一件事，留着过夜的记录是我一天的失败",
+    "created_at": "2018-12-18T05:40:51.000Z",
+    "updated_at": "2022-03-09T08:42:10.000Z"
+  },
+  "updateTime": 1683818603408
+}
+
+*/
+const getSentence = ()=>{
+    api.get('https://api.xygeng.cn/openapi/one#').then((response)=>{
+        data.oneSentence = `「 ${response.data.data.content} 」— ${response.data.data.name} ${response.data.data.origin}`
+    }).catch((err)=>{
+        console.error(err)
+        data.oneSentence = '「 API Request Error 」'
+    })
+        
+}
+
 const main = ()=>{
     typeWriterFunc();
+    getSentence()
+    setInterval(getSentence, 10000);
 }
 
 main()
@@ -26,57 +63,182 @@ main()
 </script>
 
 <template>
-    <div class="main">
-        <div ref="typewriter" class="box box0" id="content">
-            &nbsp;{{ data.bannerContent }}
+    <el-header></el-header>
+    <div class="common-main main">
+        <div ref="typewriter" class="common-box common-box0 box box0" id="content">
+            <img id="my-avatar" src="./../assets/avatar.jpeg" class="my-avatar"/>&nbsp;{{ data.bannerContent }}
         </div>
-        <div class="box box1">
+        <div class="common-box box box1">
             <a href="https://cairbin.top">
                 <i class="fa-solid fa-blog"></i>
             </a>
         </div>
-        <div class="box box2">
+        <div class="common-box box box2">
             <a href="https://github.com/CairBin">
                 <i class="fa-brands fa-github"></i>
             </a>
         </div>
-        <div class="box box3">
+        <div class="common-box box box3">
             <a href="https://blog.csdn.net/qq_42759112?spm=1000.2115.3001.5343">
                 <i class="fa-solid fa-c"></i>
             </a>
         </div>
-        <div class="box box4">
+        <div class="common-box box box4">
             <a href="https://t.me/CairBin">
                 <i class="fa-brands fa-telegram"></i>
             </a>
             
         </div>
-        <div class="box box5">
+        <div class="common-box box box5">
             <a href="https://cairbin.top/index.php/time.html">
                 <i class="fa-solid fa-envelope"></i>
             </a>
         </div>
-        <div class="box box6">
+        <div class="common-box box box6">
             <a href="https://cairbin.github.io/LearnSSM/">
                 <i class="fa-brands fa-java"></i>
             </a>
         </div>
-        <div class="box box7">
+        <div class="common-box box box7">
             <a href="https://travellings.link/">
                 <i class="fa-solid fa-train"></i>
             </a>
+        </div>
+
+        <div class="page-info">
+            <div class="info-item">{{data.oneSentence}}</div>
+            <div>&nbsp;</div>
+            <div class="info-item">&copy;&nbsp;Copyright 2024-Now CairBin</div>
+            <div class="info-item">Powered By Github Page</div>
         </div>
     </div>
 </template>
 
 <style scoped>
-.box{
-    color:#dfe4ea;
-    font-size:100px;
+
+.page-info{
+    background: #2f3542;
+    grid-column: 1/4;
+    height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color:white;
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    flex-direction: column;
+}
+
+
+.common-box{
     text-align: center;
-    line-height: 200px;
+    color:#dfe4ea;
     border: 5px solid rgba(164,176,190,0);
 }
+
+
+.common-box0{
+    grid-column: 1/4;
+    grid-row: 1/2;
+    background: #2f3542;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.box0 img{
+    width:80px;
+}
+
+
+.common-main{
+    position: absolute;
+    left:50%;
+    grid-template-columns: repeat(3,1fr);
+    grid-template-rows: repeat(5,1fr);
+    grid-gap: 10px;
+    transform: translate(-50%,-50%);
+    display: grid;
+}
+
+@media screen and (min-width:960px){
+    .box{
+        font-size:100px;
+        line-height: 200px;
+    }
+
+    .box0{
+        font-size:70px;
+    }
+
+    #my-avatar{
+        width:80px;
+    }
+
+    .main{
+        width:600px;
+        height: 200px;
+        margin-top:60px;
+    }
+}
+
+@media screen and (max-width:960px){
+    .box{
+        font-size:100px;
+        line-height: 190px;
+    }
+
+    #my-avatar{
+        width:90px;
+    }
+
+    .box0{
+        font-size: 60px;
+    }
+
+    .main{
+        height:3rem;
+        width:555px;
+    }
+}
+
+@media screen and (max-width:560px){
+    .box{
+        font-size:90px;
+        line-height: 120px;
+    }
+
+    .box0{
+        font-size: 35px;
+    }
+
+    .main{
+        height:50px;
+        width:345px;
+    }
+}
+
+@media screen and (max-width:350px){
+    .box{
+        font-size:80px;
+        line-height: 90px;
+    }
+
+    .box0{
+        font-size: 30px;
+    }
+
+    #my-avatar{
+        width:50px;
+    }
+
+    .main{
+        height:100px;
+        width:290px;
+    }
+}
+
+
 
 .box:hover{
     border:5px solid rgba(241,242,246,0.5);
@@ -84,32 +246,14 @@ main()
     cursor: pointer;
 }
 
-.main{
-    position: absolute;
-    left:50%;
-    margin-top:150px;
-    height:153px;
-    width:560px;
-    display: grid;
-    grid-template-columns: repeat(3,1fr);
-    grid-template-rows: repeat(5,1fr);
-    grid-gap: 10px;
-    transform: translate(-50%,-50%);
+.box hover::after{
+    content:'关于'
 }
 
 .box a{
     text-decoration: none;
     color:inherit;
     font:inherit;
-}
-
-.box0{
-    background: wheat;
-    grid-column: 1/4;
-    grid-row: 1/2;
-    font-size: 50px;
-    background: #2f3542;
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 }
 
 .box1{
@@ -155,22 +299,42 @@ main()
     background-color: #8e44ad;
 }
 
- /* 设置容器样式 */
-#content {
 
+ /* 设置容器样式 */
+
+/* 产生光标闪烁的效果 */
+#content::after{
+  content: '|';
+  color:darkgray;
+  animation: blink 1s infinite;
 }
-  /* 产生光标闪烁的效果 */
-  #content::after{
-      content: '|';
-      color:darkgray;
-      animation: blink 1s infinite;
+@keyframes blink{
+  from{
+      opacity: 0;
   }
-  @keyframes blink{
-      from{
-          opacity: 0;
-      }
-      to{
-          opacity: 1;
-      }
-  }
+  to{
+      opacity: 1;
+    }
+}
+
+
+.my-avatar{
+    border-radius: 50%;
+    animation: light 4s ease-in-out infinite;
+    transition: 0.5s;
+}
+
+/*头像呼吸灯动画*/
+@keyframes light {
+    0%{box-shadow: 0 0 8px #f00;}
+    25%{box-shadow: 0 0 24px #0f0;} 
+    50%{box-shadow: 0 0 8px #00f;}
+    75%{box-shadow: 0 0 24px #0f0;} 
+    100%{box-shadow: 0 0 8px #f00;}
+}
+
+/*头像旋转*/
+.my-avatar:hover {
+    transform: rotate(720deg);
+}
 </style>
